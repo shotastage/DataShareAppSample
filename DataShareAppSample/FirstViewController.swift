b8482f71b330093cd1359f7c3bd32f5315005f3f
Shota Shimazu <hornet.live.mf@gmail.com>
2019-07-12 14:33:51 +0900

    Initial Commit

---
+++ DataShareAppSample/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  DataShareAppSample
+//
+//  Created by Shota Shimazu on 2019/07/12.
+//  Copyright © 2019 Shota Shimazu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DataShareAppSample/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  DataShareAppSample
+//
+//  Created by Shota Shimazu on 2019/07/12.
+//  Copyright © 2019 Shota Shimazu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
